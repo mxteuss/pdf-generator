6ec291fe77bb706a75253ff8a1f593a4d8ab5da2
mxteuss <ovagabundant@gmail.com>
2026-02-06 02:14:05 -0300

    feat: Geração dos outros Elementos pré-textuais obrigatórios

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    document
-      .querySelector('.modern-form')
-      .addEventListener('submit', async (e) => {
-        e.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .querySelector('.modern-form')
+    .addEventListener('submit', async (e) => {
+      e.preventDefault();
 
-        const dados = {
-          curso: document.getElementById('curso').value,
-          titulo: document.getElementById('titulo').value,
-          nome: document.getElementById('nome').value,
-          instituicao: document.getElementById('instituicao').value,
-          ano: document.getElementById('ano').value,
-          cidade: document.getElementById('cidade').value,
-        };
+      const dados = {
+        curso: document.getElementById('curso').value,
+        titulo: document.getElementById('titulo').value,
+        nome: document.getElementById('nome').value,
+        instituicao: document.getElementById('instituicao').value,
+        ano: document.getElementById('ano').value,
+        cidade: document.getElementById('cidade').value,
+        tipoTrabalho: document.getElementById('tipoTrabalho').value,
+        objetivo: document.getElementById('objetivo').value,
+        orientador: document.getElementById('orientador').value,
+        dedicatoria: document.getElementById('dedicatoria').value,
+        agradecimentos: document.getElementById('agradecimentos').value,
+        epigrafe: document.getElementById('epigrafe').value,
+        resumo: document.getElementById('resumo').value,
+        palavrasChave: document.getElementById('palavrasChave').value,
+        resumoEn: document.getElementById('resumoEn').value,
+        keywords: document.getElementById('keywords').value,
+      };
 
+      try {
+        const response = await fetch('http://localhost:8080/gerar-pdf', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(dados),
+        });
 
-        try {
-          const response = await fetch('http://localhost:8080/gerar-pdf', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(dados),
-          });
-
-          if (!response.ok) {
-            console.log(`Erro HTTP! Status: ${response.status}`);
-          }
-
-          const blob = await response.blob();
-
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = 'abnt.pdf';
-          a.click();
-
-        } catch (error) {
-          console.error('ERRO:', error);
+        if (!response.ok) {
+          console.log(`Erro HTTP! Status: ${response.status}`);
         }
-      });
-  });
+
+        const blob = await response.blob();
+
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'abnt.pdf';
+        a.click();
+      } catch (error) {
+        console.error('ERRO:', error);
+      }
+    });
+});
